960b80d0ff2df868717d55893314ebbf4098f13f
Jonathan <paulwjonathan@gmail.com>
2026-01-01 11:01:15 -0500

    fix: project skeleton loading padding

--- src/app/projects/[project_name]/page.js
+++ src/app/projects/[project_name]/page.js
@@ -1,5 +1,5 @@
 'use client'
-/* eslint-disable react-hooks/set-state-in-effect */
+ 
 import { useEffect, useState, use } from 'react'
 import { EmbedCarousel } from './carousel'
 import { ProjectPageSkeleton } from '@/components/Skeletons'
@@ -42,7 +42,6 @@ export default function Details(props) {
     fetchModelUrl(project.id)
   }, [project])
 
-  if (loading) return <ProjectPageSkeleton />
   if (!project) return
 
   const { name, description, bom, web_link, project_files, project_videos } =
@@ -50,51 +49,56 @@ export default function Details(props) {
 
   return (
     <div className="mt-8 overflow-y-auto h-full w-full place-self-center max-w-4xl flex flex-col items-center gap-8 p-8">
-      {modelUrl && (
-        <div className="w-full items-center justify-center rounded-lg border border-primary/30 bg-background/80 text-sm opacity-80">
-          <ThreeViewer
-            modelUrl={modelUrl}
-            autoPlay={true}
-            resumeDelayMs={5000}
-            minDistance={0.1}
-            maxDistance={1}
-          />
-        </div>
-      )}
-      <h1 className="w-full text-primary text-2xl text-left font-bold">
-        {name}
-      </h1>
-      <div className="w-full ">
-        <h2 className="text-xl font-bold mb-4">Description</h2>
-        <p className="mb-4">{description}</p>
-        {bom && (
-          <>
-            <h2 className="text-2xl font-bold mb-4">Bill of Materials</h2>
-            <ul className="list-disc list-inside mb-4">
-              {bom.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </>
-        )}
-        {web_link && (
-          <p className="text-blue-500 dark:text-blue-300">
-            <a href={web_link} target="_blank" rel="noopener noreferrer">
-              {web_link}
-            </a>
-          </p>
-        )}
-        {project_videos && project_videos.length > 0 && (
-          <div className="mb-4">
-            <h2 className="text-2xl font-bold mb-4">{`Video${
-              project_videos.length > 1 ? 's' : ''
-            }`}</h2>
-            <EmbedCarousel
-              list={project_videos.map((e) => e.video_embed_link)}
-            />
+      {loading && <ProjectPageSkeleton />}
+      {!loading && (
+        <>
+          {modelUrl && (
+            <div className="w-full items-center justify-center rounded-lg border border-primary/30 bg-background/80 text-sm opacity-80">
+              <ThreeViewer
+                modelUrl={modelUrl}
+                autoPlay={true}
+                resumeDelayMs={5000}
+                minDistance={0.1}
+                maxDistance={1}
+              />
+            </div>
+          )}
+          <h1 className="w-full text-primary text-2xl text-left font-bold">
+            {name}
+          </h1>
+          <div className="w-full ">
+            <h2 className="text-xl font-bold mb-4">Description</h2>
+            <p className="mb-4">{description}</p>
+            {bom && (
+              <>
+                <h2 className="text-2xl font-bold mb-4">Bill of Materials</h2>
+                <ul className="list-disc list-inside mb-4">
+                  {bom.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+            {web_link && (
+              <p className="text-blue-500 dark:text-blue-300">
+                <a href={web_link} target="_blank" rel="noopener noreferrer">
+                  {web_link}
+                </a>
+              </p>
+            )}
+            {project_videos && project_videos.length > 0 && (
+              <div className="mb-4">
+                <h2 className="text-2xl font-bold mb-4">{`Video${
+                  project_videos.length > 1 ? 's' : ''
+                }`}</h2>
+                <EmbedCarousel
+                  list={project_videos.map((e) => e.video_embed_link)}
+                />
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </>
+      )}
     </div>
   )
 }
